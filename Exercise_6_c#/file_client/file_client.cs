3d2b4e995fde3883069d6178b86324187d2bbe4c
Tobias <tobiasniklassen@gmail.com>
2018-04-05 23:12:13 +0200

    done

--- Exercise_6_c#/file_client/file_client.cs
+++ Exercise_6_c#/file_client/file_client.cs
@@ -5,60 +5,98 @@ using System.Net.Sockets;
 
 namespace tcp
 {
-	class file_client
-	{
-		/// <summary>
-		/// The PORT.
-		/// </summary>
-		const int PORT = 9000;
-		/// <summary>
-		/// The BUFSIZE.
-		/// </summary>
-		const int BUFSIZE = 1000;
+    class file_client
+    {
+        /// <summary>
+        /// The PORT.
+        /// </summary>
+        const int PORT = 9000;
+        /// <summary>
+        /// The BUFSIZE.
+        /// </summary>
+        const int BUFSIZE = 1000;
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="file_client"/> class.
-		/// </summary>
-		/// <param name='args'>
-		/// The command-line arguments. First ip-adress of the server. Second the filename
-		/// </param>
-		private file_client (string[] args)
-		{
-			// TO DO Your own code
-		    TcpClient clientSocket = new TcpClient();
+        /// <summary>
+        /// Initializes a new instance of the <see cref="file_client"/> class.
+        /// </summary>
+        /// <param name='args'>
+        /// The command-line arguments. First ip-adress of the server. Second the filename
+        /// </param>
+        private file_client(string[] args)
+        {
+            // TO DO Your own code
+            TcpClient clientSocket = new TcpClient();
 
             Console.WriteLine("Client started");
             clientSocket.Connect(args[0], PORT);
             Console.WriteLine("Server connected");
 
-		    NetworkStream serverStream = clientSocket.GetStream();
-		}
+            NetworkStream networkStream = clientSocket.GetStream();
+            string fileName = args[1];
 
-		/// <summary>
-		/// Receives the file.
-		/// </summary>
-		/// <param name='fileName'>
-		/// File name.
-		/// </param>
-		/// <param name='io'>
-		/// Network stream for reading from the server
-		/// </param>
-		private void receiveFile (String fileName, NetworkStream io)
-		{
-			// TO DO Your own code
+            LIB.writeTextTCP(networkStream, fileName);
 
-		}
+            receiveFile(fileName, networkStream);
 
-		/// <summary>
-		/// The entry point of the program, where the program control starts and ends.
-		/// </summary>
-		/// <param name='args'>
-		/// The command-line arguments.
-		/// </param>
-		public static void Main (string[] args)
-		{
-			Console.WriteLine ("Client starts...");
-			new file_client(args);
-		}
-	}
+            clientSocket.Close();
+            networkStream.Close();
+        }
+
+        /// <summary>
+        /// Receives the file.
+        /// </summary>
+        /// <param name='fileName'>
+        /// File name.
+        /// </param>
+        /// <param name='io'>
+        /// Network stream for reading from the server
+        /// </param>
+        private void receiveFile(String fileName, NetworkStream io)
+        {
+            // TO DO Your own code
+            string filename = LIB.extractFileName(fileName);
+            long fileSize = LIB.getFileSizeTCP(io);
+            FileStream fs = new FileStream("/root/Downloads/" + filename, FileMode.Create, FileAccess.Write);
+            byte[] recieveBytes = new byte[fileSize];
+            int recieved = 0;
+
+            if (fileSize == 0)
+            {
+                Console.WriteLine("File do not exist on server");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("File size: " + fileSize);
+                while (recieved < fileSize)
+                {
+                    if ((fileSize - recieved) > BUFSIZE)
+                    {
+                        io.Read(recieveBytes, 0, BUFSIZE);
+                        fs.Write(recieveBytes, 0, BUFSIZE);
+                        recieved += BUFSIZE;
+                    }
+                    else
+                    {
+                        int read = io.Read(recieveBytes, 0, ((int)fileSize - recieved));
+                        fs.Write(recieveBytes, 0, read);
+                        recieved += read;
+                    }
+                    Console.WriteLine("Bytes recieved: " + recieved);
+                }
+            }
+        }
+
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name='args'>
+        /// The command-line arguments.
+        /// </param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Client starts...");
+            new file_client(args);
+        }
+    }
 }
--- Exercise_6_c#/file_server/file_server.cs
+++ Exercise_6_c#/file_server/file_server.cs
@@ -5,110 +5,120 @@ using System.Net.Sockets;
 
 namespace tcp
 {
-	class file_server
-	{
-		/// <summary>
-		/// The PORT
-		/// </summary>
-		const int PORT = 9000;
-		/// <summary>
-		/// The BUFSIZE
-		/// </summary>
-		const int BUFSIZE = 1000;
+    class file_server
+    {
+        /// <summary>
+        /// The PORT
+        /// </summary>
+        const int PORT = 9000;
+        /// <summary>
+        /// The BUFSIZE
+        /// </summary>
+        const int BUFSIZE = 1000;
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="file_server"/> class.
-		/// Opretter en socket.
-		/// Venter på en connect fra en klient.
-		/// Modtager filnavn
-		/// Finder filstørrelsen
-		/// Kalder metoden sendFile
-		/// Lukker socketen og programmet
- 		/// </summary>
-		private file_server ()
-		{
-			// TO DO Your own code
+        /// <summary>
+        /// Initializes a new instance of the <see cref="file_server"/> class.
+        /// Opretter en socket.
+        /// Venter på en connect fra en klient.
+        /// Modtager filnavn
+        /// Finder filstørrelsen
+        /// Kalder metoden sendFile
+        /// Lukker socketen og programmet
+        /// </summary>
+        private file_server()
+        {
+            // TO DO Your own code
 
-            TcpListener serverSocket = new TcpListener(PORT);
+            TcpListener serverSocket = new TcpListener(IPAddress.Any, PORT);
             serverSocket.Start();
             Console.WriteLine("Server started");
 
 
             while (true)
-		    {
-		        try
-		        {
-		            TcpClient clientSocket = serverSocket.AcceptTcpClient();
-		            Console.WriteLine("Accept connection from client");
+            {
+                try
+                {
+                    TcpClient clientSocket = serverSocket.AcceptTcpClient();
+                    Console.WriteLine("Accept connection from client");
                     NetworkStream networkStream = clientSocket.GetStream();
-		            string fileName = LIB.readTextTCP(networkStream);
-		            long fileSize = LIB.check_File_Exists(fileName);
+                    string fileName = LIB.readTextTCP(networkStream);
+                    long fileSize = LIB.check_File_Exists(fileName);
 
-		            if (fileSize == 0)
-		            {
-		                Console.WriteLine("File does not exist");
-		            }
-		            else
-		            {
-		                LIB.writeTextTCP(networkStream, fileSize.ToString());
+                    if (fileSize == 0)
+                    {
+                        Console.WriteLine("File does not exist");
+                    }
+                    else
+                    {
+                        LIB.writeTextTCP(networkStream, fileSize.ToString());
 
-		                sendFile(fileName, fileSize, networkStream);
+                        sendFile(fileName, fileSize, networkStream);
 
-		                Console.WriteLine("File send");
+                        Console.WriteLine("File send");
                     }
 
                     clientSocket.Close();
                     networkStream.Close();
 
-		        }
-		        catch (Exception e)
-		        {
-		            Console.WriteLine(e.ToString());
-		        }
-		    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
 
 
-		}
+        }
 
-		/// <summary>
-		/// Sends the file.
-		/// </summary>
-		/// <param name='fileName'>
-		/// The filename.
-		/// </param>
-		/// <param name='fileSize'>
-		/// The filesize.
-		/// </param>
-		/// <param name='io'>
-		/// Network stream for writing to the client.
-		/// </param>
-		private void sendFile (String fileName, long fileSize, NetworkStream io)
-		{
-			// TO DO Your own code
+        /// <summary>
+        /// Sends the file.
+        /// </summary>
+        /// <param name='fileName'>
+        /// The filename.
+        /// </param>
+        /// <param name='fileSize'>
+        /// The filesize.
+        /// </param>
+        /// <param name='io'>
+        /// Network stream for writing to the client.
+        /// </param>
+        private void sendFile(String fileName, long fileSize, NetworkStream io)
+        {
+            // TO DO Your own code
             FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
             byte[] sendBytes = new byte[BUFSIZE];
-		    int send = 0;
+            int send = 0;
 
-		    while (send < fileSize)
-		    {
-		        int read = fs.Read(sendBytes, 0, BUFSIZE);
-		        io.Write(sendBytes, 0, read);
-		        send += read;
-		    }
+            while (send < fileSize)
+            {
+                if ((fileSize - send) > BUFSIZE)
+                {
+                    fs.Read(sendBytes, 0, BUFSIZE);
+                    io.Write(sendBytes, 0, BUFSIZE);
+                    send += BUFSIZE;
+                }
+                else
+                {
+                    int read = fs.Read(sendBytes, 0, ((int)fileSize - send));
+                    io.Write(sendBytes, 0, read);
+                    send += read;
+                }
+                Console.WriteLine("Bytes send: " + send);
+            }
 
             fs.Close();
-		}
+        }
 
-		/// <summary>
-		/// The entry point of the program, where the program control starts and ends.
-		/// </summary>
-		/// <param name='args'>
-		/// The command-line arguments.
-		/// </param>
-		public static void Main (string[] args)
-		{
-			Console.WriteLine ("Server starts...");
-			new file_server();
-		}
-	}
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name='args'>
+        /// The command-line arguments.
+        /// </param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Server starts...");
+            new file_server();
+        }
+    }
 }
